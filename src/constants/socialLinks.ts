a56535aecc6b7e3247079d013e3b0644836bfd77
Luis Álvarez <luis.alvarez.developer@gmail.com>
2026-04-17 18:34:04 -0400

    refactor: modificado hero sectio

---
+++ src/constants/socialLinks.ts
@@ -0,0 +1,22 @@
+export const socialLinks = [
+  {
+    name: "GitHub",
+    href: "https://github.com/luis-alvarezf10",
+    icon: "hn hn-github"
+  },
+  {
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/luis-%C3%A1lvarez-franceschi-a2080b225/",
+    icon: "hn hn-linkedin"
+  },
+  {
+    name: "X",
+    href: "https://x.com/luis_alvarezf10",
+    icon: "hn hn-x"
+  },
+  {
+    name: "Instagram",
+    href: "https://www.instagram.com/luis_alvarezf10",
+    icon: "hn hn-instagram"
+  }
+];
