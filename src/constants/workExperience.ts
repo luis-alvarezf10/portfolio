69bc9ae25b65a87788fb4f2fd169be58a62b42a4
Luis Álvarez <luis.alvarez.developer@gmail.com>
2026-04-17 20:28:24 -0400

    refactor -> workExperience

---
+++ src/constants/workExperience.ts
@@ -0,0 +1,21 @@
+// src/constants/workExperience.ts
+export const workExperience = [
+  {
+    company: "Nex Technology",
+    date: "Marzo 2025 - presente",
+    description: "Desarrollo software, páginas web, sistemas de gestión.",
+    icon: "hn hn-startups",
+  },
+  {
+    company: "Medizin",
+    date: "Septiembre 2025 - abril 2026",
+    description: "Desarrollo de aplicaciones de escritorio, conexión APIs y optimización UI/UX.",
+    icon: "hn hn-business",
+  },
+  {
+    company: "Hunter",
+    date: "Febrero 2026 - presente",
+    description: "Desarrollo de sistemas de gestión web y móvil, automatización con IA. ",
+    icon: "hn hn-startups",
+  },
+];
